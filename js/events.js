000b7b62741ae571794afd3fff7f8d7f11bd52c6
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-28 22:24:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,13 +11,19 @@ function frameIt(){
 }
 function pressIt(){
   $("form").on('keydown', function(key) {
-  if(key.which == 71){
+    if(key.which == 71){
       alert('g was pressed');
-  }
-});
+    }
+  });
+}
+function submitIt(){
+  $("form").on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
 }
 $(document).ready(function(){
 getIt()
 frameIt()
 pressIt()
+submitIt()
 });
